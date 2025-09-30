c7ca1ca837da0223f37a2c209abbff04d5499341
Vikas Vishwakarma <vikasweb77@gmail.com>
2025-09-30 11:40:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Get Minimum Squares/get-minimum-squares.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int MinSquares(int n) {
+        // Code here
+        vector<int> dp(n+1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            int ans = INT_MAX;
+            for(int j = 1; j*j<= i; j++) {
+                int rem = i - j*j;
+                if(dp[rem] < ans) {
+                    ans = dp[rem];
+                }
+            }
+            dp[i] = ans + 1;
+        }
+        return dp[n];
+    }
+};
